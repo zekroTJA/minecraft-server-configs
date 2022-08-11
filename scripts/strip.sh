fb401b5431e5551b7f056abccec7e2e39dac65d2
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2022-08-11 15:25:46 +0200

    update scripts

--- scripts/strip.sh
+++ scripts/strip.sh
@@ -2,9 +2,13 @@ REMOVE=(
     dashboards
     scripts
     .gitignore
+    crontab
     desec-rrsets.json
     docker-compose.dev.yml
     Taskfile.yaml
 )
 
+set -ex
+
 rm -rf ${REMOVE[@]}
+mv example.rclone.conf rclone.conf
